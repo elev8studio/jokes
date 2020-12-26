2136fc5cca85efead8efb7f660aa6bd7d1f7149e
Jonathan Smithies <jonathansmithies@Jonathans-iMac.local>
2020-12-26 18:46:55 +0000

    Added back route middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,11 +39,13 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::middleware('api')
+                ->prefix('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::namespace($this->namespace)
+            Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
